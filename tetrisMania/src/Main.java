dc8a9344a6b384fc81e746c84c94a0f96a20cf3b
aKijn2 <mrkinder20041010@gmail.com>
2024-01-31 08:29:22 +0100

    window

--- tetrisMania/src/Main.java
+++ tetrisMania/src/Main.java
@@ -2,7 +2,12 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         JFrame windows = new JFrame("Tetris Mania");
+        windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windows.setResizable(false);
+        
+        windows.setLocationRelativeTo(null);
+        windows.setVisible(true);
     }
 }
